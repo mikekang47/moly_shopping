f0f62f6cb30289515e421efc422835f043882ea7
mikekang47 <gyeong0308@gmail.com>
2022-01-06 01:06:01 +0900

    Feat: set environment

---
+++ src/main/java/com/solidcommerce/moly_shopping/MolyShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.solidcommerce.moly_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolyShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MolyShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solidcommerce/moly_shopping/MolyShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solidcommerce.moly_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolyShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
