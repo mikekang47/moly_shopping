ba23dd6beec4b3a70423798fbcc715be83fd4846
mikekang47 <gyeong0308@gmail.com>
2022-01-07 01:22:19 +0900

    Feat: make Repository class

---
+++ src/main/java/com/solidcommerce/moly_shopping/application/ProductService.java
@@ -0,0 +1,22 @@
+package com.solidcommerce.moly_shopping.application;
+
+import com.solidcommerce.moly_shopping.domain.Product;
+import com.solidcommerce.moly_shopping.domain.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+}
---
+++ src/main/java/com/solidcommerce/moly_shopping/infra/ProductJpaRepository.java
@@ -0,0 +1,4 @@
+package com.solidcommerce.moly_shopping.infra;
+
+public class ProductJpaRepository {
+}
