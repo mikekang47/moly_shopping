f5a7c03fe6ff2676aec12c7f00571b962bd8d454
mikekang47 <gyeong0308@gmail.com>
2022-01-07 23:55:37 +0900

    Refactor: change class name

--- src/main/java/com/solidcommerce/moly_shopping/infra/JpaProductRepository.java
+++ src/main/java/com/solidcommerce/moly_shopping/infra/JpaProductRepository.java
@@ -3,7 +3,6 @@ package com.solidcommerce.moly_shopping.infra;
 import com.solidcommerce.moly_shopping.domain.Product;
 import com.solidcommerce.moly_shopping.domain.ProductRepository;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +10,7 @@ import java.util.List;
 
 @Repository
 @Primary
-public interface ProductJpaRepository
-    extends ProductRepository, CrudRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+public interface JpaProductRepository
+    extends ProductRepository, CrudRepository<Product, Long>{
     List<Product> findAll();
 }
