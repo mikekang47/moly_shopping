15aa1eac7a30e7178351fa39430d84889bc2af43
mikekang47 <gyeong0308@gmail.com>
2022-01-07 23:52:12 +0900

    Feat: make ProductJpaRepository

--- src/main/java/com/solidcommerce/moly_shopping/infra/ProductJpaRepository.java
+++ src/main/java/com/solidcommerce/moly_shopping/infra/ProductJpaRepository.java
@@ -1,4 +1,17 @@
 package com.solidcommerce.moly_shopping.infra;
 
-public class ProductJpaRepository {
+import com.solidcommerce.moly_shopping.domain.Product;
+import com.solidcommerce.moly_shopping.domain.ProductRepository;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Primary
+public interface ProductJpaRepository
+    extends ProductRepository, CrudRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    List<Product> findAll();
 }
